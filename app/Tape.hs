14e7b3e16b1282b4b521d235b21ebbb564a6c9c0
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-05-15 21:19:02 +0100

    feat: add readChar function to Tape module for reading character input

--- app/Tape.hs
+++ app/Tape.hs
@@ -7,7 +7,8 @@ module Tape
     dec,
     printCell,
     showTape,
-    getCellValue
+    getCellValue,
+    readChar
   )
 where
 
@@ -18,9 +19,11 @@ initialTape = (repeat 0, 0, repeat 0)
 
 moveRight :: Tape -> Tape
 moveRight (ls, x, r : rs) = (x : ls, r, rs)
+moveRight _ = error "moveRight: right list unexpectedly empty"
 
 moveLeft :: Tape -> Tape
 moveLeft (l : ls, x, rs) = (ls, l, x : rs)
+moveLeft _ = error "moveLeft: left list unexpectedly empty"
 
 inc :: Tape -> Tape
 inc (ls, x, rs) = (ls, (x + 1) `mod` 256, rs)
@@ -33,6 +36,11 @@ printCell (_, x, _) = putChar (toEnum x)
 
 getCellValue :: Tape -> Int
 getCellValue (_, val, _) = val
+
+readChar :: Tape -> IO Tape
+readChar (ls, _, rs) = do
+  char <- getChar -- Read a single character from stdin
+  return (ls, fromEnum char, rs) -- Convert char to its ASCII value and store it
 
 showTape :: Tape -> String
 showTape (ls, x, rs) =
